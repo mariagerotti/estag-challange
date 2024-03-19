d952e1473735c9c4759508925e449d069b22a637
mariagerotti <mariaeduarda.gerotti@gmail.com>
2024-03-19 09:09:52 -0300

    finish: end react

--- front/src/lib/axios.js
+++ front/src/lib/axios.js
@@ -1,8 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  // baseURL: 'http://127.0.0.1:33625'
-  baseURL: 'http://127.0.0.1:80' // local
+  baseURL: 'http://127.0.0.1:80'
 });
 
 export default instance;
