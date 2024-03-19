8eddd0ca435071cbcf6f0f9921f9f7a76a8645ac
mariagerotti <mariaeduarda.gerotti@gmail.com>
2024-03-19 09:08:59 -0300

    att final react

--- front/src/components/HomeComponents/TrHome.jsx
+++ front/src/components/HomeComponents/TrHome.jsx
@@ -1,34 +1,29 @@
 import DeleteButton from "../DeleteButton/DeleteButton";
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 import { deleteProductFromCart } from "../../redux/cart/actions";
 
 const TrHome = ({ product }) => {
   const total = product.price * product.amount;
 
   const dispatch = useDispatch();
-  const { products } = useSelector((rootReducer) => rootReducer.cartReducer);
 
-
-  console.table(products);
   const deleteProductButton = async (code) => {
     dispatch(deleteProductFromCart(code));
   };
 
   return (
     <>
-      {products.map((product) => (
-        <tr key={product.code}>
-          <td>{product.code}</td>
-          <td>{product.name}</td>
-          <td>{product.amount}</td>
-          <td>{product.price}</td>
-          <td>{product.categoryName}</td>
-          <td>{total}</td>
-          <td>
-            <DeleteButton onclick={() => deleteProductButton(product.code)} />
-          </td>
-        </tr>
-      ))}
+      <tr key={product.code}>
+        <td>{product.code}</td>
+        <td>{product.name}</td>
+        <td>{product.amount}</td>
+        <td>{product.price}</td>
+        <td>{product.categoryName}</td>
+        <td>{total}</td>
+        <td>
+          <DeleteButton onclick={() => deleteProductButton(product.code)} />
+        </td>
+      </tr>
     </>
   );
 };
--- front/src/redux/store.js
+++ front/src/redux/store.js
@@ -2,6 +2,10 @@ import { createStore, applyMiddleware } from "redux";
 import logger from "redux-logger";
 import rootReducer from "./rootReducer";
 
-const store = createStore(rootReducer, applyMiddleware(logger));
+const store = createStore(
+  rootReducer,
+//   applyMiddleware(logger),
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 export default store;
